be77f18a25fe648cb270b67a2f705f88e06c270a
Adel-Yousef <adelyusif9@gmail.com>
2025-10-27 13:35:54 +0300

    Did the serializers file and wrote the needed serializers

--- main_app/serializers.py
+++ main_app/serializers.py
@@ -2,3 +2,21 @@ from rest_framework import serializers
 from .models import Challenge, Participation, Progress
 
 
+class ProgressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Progress
+        fields = '__all__'
+
+class ParticipationSerializer(serializers.ModelSerializer):
+    Progress = ProgressSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Participation
+        fields = '__all__'
+
+class ChallengeSerializer(serializers.ModelSerializer):
+    Participations = ParticipationSerializer(many=True, read_only=True)
+    
+    class Meta:
+        model = Challenge
+        fields = '__all__'
