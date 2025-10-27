1a1447c6aa05f4ba02a3ce78b9ec8d3690cf6323
Adel-Yousef <adelyusif9@gmail.com>
2025-10-27 13:33:50 +0300

    Did the models including the user which is built in django with the correct relationships

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+User = get_user_model()
+
+class Challenge(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=500)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    created_by = models.ForeignKey(User,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+    
+class Participation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    challenge = models.ForeignKey(Challenge, on_delete=models.CASCADE, related_name='participations')
+    join_date = models.DateField('Join date')
+
+    def __str__(self):
+        return f'{self.user.username} - {self.challenge.title}'
+    
+    class Meta:
+        ordering = ['-join_date']
+
+PROGRESS_STATUS = (
+    ('D', 'Done'),
+    ('S', 'Skipped'),
+    ('P', 'Partial')
+)
+
+class Progress(models.Model):
+    date = models.DateField('Progress date')
+    status = models.CharField(max_length=1, choices=PROGRESS_STATUS, default=PROGRESS_STATUS[0][0])
+    participation = models.ForeignKey(Participation, on_delete=models.CASCADE, related_name='progress')
+
+    def __str__(self):
+        return f'{self.get_status_display()} - {self.date}'
+    
+    class Meta:
+        ordering = ['-date']
