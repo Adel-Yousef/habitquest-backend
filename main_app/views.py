9da1e990eaf6f9c7e387ccc20e0feb5e57803ffe
Adel-Yousef <adelyusif9@gmail.com>
2025-10-29 11:48:18 +0300

    Did the ParticipationProgress so the user can log daily progress and view the progress and wrote the path and test it in postman

--- main_app/views.py
+++ main_app/views.py
@@ -60,6 +60,24 @@ class ChallengeDetail(APIView):
             return Response({'error': str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
     
 
+class ParticipationProgress(APIView):
+    def get(self, request, participation_id):
+        queryset = Progress.objects.filter(participation=participation_id)
+        serializer = ProgressSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def post(self, request, participation_id):
+        try:
+            serializer = ProgressSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                queryset = Progress.objects.filter(participation=participation_id)
+                many_serializer = ProgressSerializer(queryset, many=True)
+                return Response(many_serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as error:
+            return Response({'error': str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
 class LeaveChallenge(APIView):
     def delete(self, request, challenge_id, user_id):
         try:
